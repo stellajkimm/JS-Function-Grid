7a1bb0a6fd112660ebec5a24b282738e12458ce8
Stella Kim <stellajkimm@gmail.com>
2017-02-11 23:33:37 +0000

    move setColor function from html file to js file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function setColor(row, col, color) {
+    var cell = $("#t")[0].rows[row].cells[col];
+    cell.style.backgroundColor = color;
+}
+             
 //Write your function calls below this line
